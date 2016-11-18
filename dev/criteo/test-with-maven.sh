c1c4a22d2b31abcd153cce70e39be822beaba79d
Anthony Truchet <a.truchet@criteo.com>
2016-11-18 16:51:19 +0100

    [dev][criteo] Add a Travis configuration script
    The script is copied from Spark 2, just the test command is adapted

--- dev/criteo/test-with-maven.sh
+++ dev/criteo/test-with-maven.sh
@@ -8,7 +8,8 @@ source `dirname "$0"`/criteo-spark-env.sh
 
 export _JAVA_OPTIONS="-Xss2048k -Dspark.buffer.pageSize=1048576 -Xmx4g"
 
-SPARK_TEST_TAGS_EXCLUDED=org.apache.spark.tags.DockerTest
+SPARK_TEST_TAGS_EXCLUDED_DEFAULT=org.apache.spark.tags.DockerTest
+SPARK_TEST_TAGS_EXCLUDED=${SPARK_TEST_TAGS_EXCLUDED-${SPARK_TEST_TAGS_EXCLUDED_DEFAULT}}
 
 $SPARK_HOME/build/mvn $CRITEO_SPARK_PROFILES                                   \
     -Dtest.exclude.tags=$SPARK_TEST_TAGS_EXCLUDED                              \
