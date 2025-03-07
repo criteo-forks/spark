1c1fa201af09b7aa05d516aa3b910f37b0e0cf19
d.dahmane <d.dahmane@criteo.com>
2025-03-07 15:50:16 +0100

    build: upload jars using deploy:deploy instead of deploy only

--- external/docker/criteo-build/build_script.sh
+++ external/docker/criteo-build/build_script.sh
@@ -110,19 +110,14 @@ mvn deploy:deploy-file \
     ${MVN_COMMON_DEPLOY_FILE_PROPERTIES}
 
 # jar artifacts (for parent poms) deployment
-mvn deploy \
+mvn deploy:deploy \
     --batch-mode \
     ${MVN_COMMON_PROPERTIES} \
     -Phadoop-provided \
     -DaltDeploymentRepository=criteo::default::${NEXUS_ARTIFACT_URL} \
     -Dcriteo.repo.username=${MAVEN_USER} \
     -Dcriteo.repo.password=${MAVEN_PASSWORD} \
-    -DskipTests \
-    -Dmaven.javadoc.skip=true \
-    -Dmaven.scaladoc.skip=true \
-    -Dmaven.source.skip \
-    -Dcyclonedx.skip=true
-
+    -DskipTests
 
 # python deployment
 deploy_python $PYTHON_PEX_VERSION
